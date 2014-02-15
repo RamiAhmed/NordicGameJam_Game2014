c232698818a9ff7647574c112d6dbb76c94e182c
Andrea Distler <andrea.distler@gmail.com>
2014-02-15 15:49:27 +0100

    added main menu

---
+++ Game/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour
+{
+
+	void OnMouseDown()
+	{
+		Application.Quit();
+	}
+}
---
+++ Game/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour
+{
+	void OnMouseDown()
+	{
+		Application.LoadLevel(1);
+	}
+}
