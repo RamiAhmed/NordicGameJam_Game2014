b6ebba12003e52d70707dd14c8826eb1895fcb04
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2014-02-15 23:47:24 +0100

    Removed Movement script
    - Merged it into PlayerMovement instead

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : Movement
-{
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour {
+	public float FloorMoveSpeed = 10;
+	public float Deceleration = 5;
+	public float TurnSpeed = 2.5f;
+
+	private const float deadZone = 0.25f;
+
 	void Update() {
 		if (!this.transform.root.GetComponent<PlayerController>().IsDead) {
-			Move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+			Vector3 movementDir = Vector3.zero;
+
+			Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+			
+			if (input.magnitude > deadZone)
+			{
+				input = input.normalized * ((input.magnitude - deadZone) / (1 - deadZone));
+				
+				movementDir = transform.right * input.x + transform.forward * input.y;
+				
+				movementDir.y = 0;
+				movementDir.Normalize();
+				
+				// turn to look forward
+				transform.forward = Vector3.Lerp(transform.forward, movementDir, Time.deltaTime * TurnSpeed);
+				
+				// move
+				rigidbody.AddForce(movementDir * FloorMoveSpeed);
+
+				if (!animation.IsPlaying("PlayerRunning")) 
+					animation.Play("PlayerRunning");
+			}
+			else
+			{
+				// slow down when not pressing any directional buttons
+				Vector3 v = rigidbody.velocity;
+				v.x = Mathf.Lerp(v.x, 0, Time.deltaTime * Deceleration);
+				v.z = Mathf.Lerp(v.z, 0, Time.deltaTime * Deceleration);
+				rigidbody.velocity = v;
+				
+				// ignore collision-created turning
+				rigidbody.angularVelocity = Vector3.zero;
+
+				if (rigidbody.velocity.magnitude <= 0.1f) {
+					if (animation.IsPlaying("PlayerRunning")) 
+						animation.Play("PlayerRunning");
+				}
+			}
 		}
 	}
 }
