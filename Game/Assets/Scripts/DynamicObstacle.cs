689fb2dd83a5707118ce1bdd1c64135faa679a41
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2014-02-15 04:01:30 +0100

    Added faulty spawning of dynamic obstacles
    - Spawn X number at beginning
    * Spawn within a large circle, remove ones within a set radius
    - Spawn every Y seconds (currently spawns multiple dynamic objects
    simultaneously)

--- Game/Assets/Scripts/DynamicObstacle.cs
+++ Game/Assets/Scripts/DynamicObstacle.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class DynamicObstacle : MonoBehaviour {
 
+	public float KillY = -10f;
+
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log("Starting dynamic obstacle at: " + this.transform.position.ToString() + " at time: " + GameController.Instance.GameTime.ToString("F1"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (this.transform.position.y < KillY) {
+			RemoveSelf();
+		}
+	}
+
+	public void RemoveSelf() {
+		GameController.Instance.DynamicObstacles.Remove(this);
+		Destroy(this.gameObject);
 	}
 }
--- Game/Assets/Scripts/GameController.cs
+++ Game/Assets/Scripts/GameController.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
+
+	public GameObject Player = null;
+	private PlayerController _player;
+
+	public float SpawnRadiusFromPlayer = 50f;
+
+	public int InitialAmountOfDynamicObstacles = 15;
+
+	public List<DynamicObstacle> DynamicObstacles = null;
+
+	public float GameTime = 0f;
+	public int SpawnInterval = 15;
 
 	#region GameController Singleton Pattern
 	public static string PrefabPathAndName = "GameController";
@@ -34,7 +47,7 @@ public class GameController : MonoBehaviour {
 
 	void Awake() {
 		if (_instance != null && _instance != this) {
-			DestroyImmediate(this.gameObject);
+			Destroy(this.gameObject);
 			Debug.Log("Removing duplicated GameController Singleton");
 		}
 		else {
@@ -55,11 +68,55 @@ public class GameController : MonoBehaviour {
 		if (!_initialized) {
 			_initialized = true;
 			Debug.Log("GameController initializing");
+
+			DynamicObstacles = new List<DynamicObstacle>();
+
+			for (int i = 0; i < InitialAmountOfDynamicObstacles; i++) {
+				Invoke("spawnDynamicObstacle", (float)i * 0.1f);
+			}
+
+			if (Player != null) {
+				_player = Player.GetComponent<PlayerController>();
+				if (_player == null) {
+					Debug.LogError("Could not find PlayerController component on Player: " + Player.ToString());
+				}
+			}
+			else {
+				Debug.LogError("Player oject has not been set on GameController");
+			}
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!_initialized || _player == null) {
+			return;
+		}
+
+
+		// Iterate the game time
+		GameTime += Time.deltaTime;
+
+		if (Mathf.RoundToInt(GameTime) % SpawnInterval == 0) {
+			spawnDynamicObstacle();
+		}	
+
+	}
+
+	void OnGUI() {
+		GUI.Box (new Rect(5f, 5f, 100f, 50f), new GUIContent("Time: " + GameTime.ToString("F1")));
+	}
+
+	private void spawnDynamicObstacle() {
+		Debug.Log("Spawning dynamic obstacle. GameTime: " + GameTime.ToString("F1"));
+
+		DynamicObstacle dynamicObstacle = (Instantiate(Resources.Load("DynamicObstacle")) as GameObject).GetComponent<DynamicObstacle>();
+		DynamicObstacles.Add(dynamicObstacle);
+
+		dynamicObstacle.transform.position = _player.transform.position + (new Vector3(Random.value, 0f, Random.value) * SpawnRadiusFromPlayer * 2f);
+
+		if (Vector3.Distance(_player.transform.position, dynamicObstacle.transform.position) < SpawnRadiusFromPlayer) {
+			dynamicObstacle.RemoveSelf();
+		}
 	}
 }
