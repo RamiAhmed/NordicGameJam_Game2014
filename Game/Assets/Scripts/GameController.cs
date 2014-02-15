7b16635450ea3589a61b6e5e7df141664eb5a2dd
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2014-02-15 03:18:59 +0100

    Added Singleton pattern to GameController
    - Added empty player script
    - Added standard assets Third Person Controller & Third Person Camera
    (just to start with something)
    - Added a tag for dynamic obstacles
    - Added an empty game object for dynamic obstacles

--- Game/Assets/Scripts/GameController.cs
+++ Game/Assets/Scripts/GameController.cs
@@ -3,9 +3,59 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+	#region GameController Singleton Pattern
+	public static string PrefabPathAndName = "GameController";
+
+	private static GameController _instance = null;
+	private static bool _initialized = false;
+
+	public static GameController Instance {
+		get {
+			if (_instance == null) {
+				_instance = (Instantiate(Resources.Load(PrefabPathAndName)) as GameObject).GetComponent<GameController>();
+				if (_instance != null) {
+					_instance.Initialize();
+					return _instance;
+				}
+				else {
+					Debug.LogError("Could not instantiate GameController object. Looking at path and name:" + PrefabPathAndName);
+					return null;
+				}
+			}
+			else if (_instance != null) {
+				return _instance;
+			}
+			else {
+				Debug.LogError("Could not find the GameController. Looked at path: " + PrefabPathAndName);
+				return null;
+			}
+		}
+	}
+
+	void Awake() {
+		if (_instance != null && _instance != this) {
+			DestroyImmediate(this.gameObject);
+			Debug.Log("Removing duplicated GameController Singleton");
+		}
+		else {
+			Debug.Log("GameController awaking");
+			_instance = this;
+			DontDestroyOnLoad(this.gameObject);
+		}
+	}
+
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("Starting the GameController");
+		Initialize();
+	}
+	#endregion
+
+	void Initialize() {
+		if (!_initialized) {
+			_initialized = true;
+			Debug.Log("GameController initializing");
+		}
 	}
 	
 	// Update is called once per frame
